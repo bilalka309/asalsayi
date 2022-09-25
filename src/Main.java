caa7e013dbd8d1382519e748614796d7141e9787
bilal karahan <bilalkarahan309@gmail.com>
2022-09-25 08:18:52 +0300

    s

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*for(int i=1;i<=100;i++){
+            if(i==1){}else{
+            if(i==2){
+                System.out.print(i+" ");
+            }else if (i%2==1){
+                if(i==3){
+                    System.out.print(i+" ");
+                } else if (i % 3 != 0) {
+                    if(i==5){
+                        System.out.print(i+" ");
+                    } else if (i % 5 != 0) {
+                        if(i==7){
+                            System.out.print(i+" ");
+                        }else if (i % 7 != 0) {
+                            System.out.print(i+" ");
+                        }
+                    }
+                }
+            }
+        }
+        }
+         */
+        String asalSayilar = "";
+        for (int i = 1; i <= 100; i++) {
+            int sayac = 0;
+            for (int j = i; j >= 1; j--) {
+                if (i % j == 0) {
+                    sayac ++;
+                }
+            }
+            if (sayac == 2) {
+                asalSayilar = asalSayilar + i + " ";
+            }
+        }
+        System.out.println(asalSayilar);
     }
 }
